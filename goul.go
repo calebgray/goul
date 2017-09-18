4e3cb291df25eb8656230f0d3168bc412a766e23
Caleb Gray <calebgray@level11.com>
2017-09-18 15:48:55 -0700

    More CI improvements.

--- goul.go
+++ goul.go
@@ -6,20 +6,56 @@ import (
 	"flag"
 	"github.com/calebgray/goul/arguments"
 	"os"
+	"os/exec"
+	"runtime"
 )
 
 func main() {
 	arguments.AddCommand("build", func(args []string) int {
-		println("`command -v git > /dev/null && git rev-parse --short HEAD || echo undefined`")
+		var output []byte
+		var err error
+		output, err = exec.Command("git", []string{"rev-parse", "--short", "HEAD"}...).Output()
+		version := "none"
+		if err == nil {
+			version = string(output[:7])
+		}
+
+		/*files, err := ioutil.ReadDir(".")
+		if err != nil {
+			return 1
+		}*/
+		files := []string{"unzip"}
+		progress := 0
+		progressIncrement := 100 / len(files)
+		outArg := "-o=bin/" + version + "/"
+		for _, file := range files {
+			progress += progressIncrement
+			/*if !file.IsDir() {
+				continue
+			}*/
+			outFile := outArg + file
+			if runtime.GOOS == "windows" {
+				outFile += ".exe"
+			}
+			exec.Command("go", []string{"build", outFile, "./" + file}...).Run()
+			err = exec.Command(outFile).Run()
+			if err == nil {
+				print(progress, "%\n")
+			} else {
+				os.Remove(outFile)
+			}
+		}
+
 		return 0
 	})
 
-	if arguments.Run() == 0 {
+	rc := arguments.Run()
+	if rc == 0 {
 		os.Exit(0)
 	}
 
 	for _, arg := range flag.Args() {
 		println("Unknown argument:", arg)
 	}
-	os.Exit(1)
+	os.Exit(rc)
 }
